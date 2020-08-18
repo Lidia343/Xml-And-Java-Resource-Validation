e6e59540db385921f3d24c92ebb91b80f5cc1aa7
Sorcerer <Sorcerer@LAPTOP-G4MS948M>
2020-08-18 15:36:07 +0300

    Изменено сообщение об ошибке и имя лаунчера.

--- dcmdon.resources.validation/src/dcmdon/resources/validation/main/Application.java
+++ dcmdon.resources.validation/src/dcmdon/resources/validation/main/Application.java
@@ -2,20 +2,25 @@ package dcmdon.resources.validation.main;
 
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
-
 import dcmdon.resources.validation.ResourceValidator;
 
 public class Application implements IApplication 
 {
+	private static final String PLUGIN_ID = "dcmdon.resources.validation";
+	private static final String ERROR_MESSAGE = "Укажите путь к исполняемому файлу " +
+												"(" + PLUGIN_ID + ".exe), а также путь " +
+												"к файлу конфигурации (как первый " +
+												"аргумент команды).";
 	@Override
 	public Object start(IApplicationContext a_context)
 	{
 		try
 		{
-			String [] args = (String [])a_context.getArguments().get(IApplicationContext.APPLICATION_ARGS);
+			String [] args = (String [])a_context.getArguments().get(
+							  IApplicationContext.APPLICATION_ARGS);
 			if (args.length == 0)
 			{
-				System.out.println(ResourceValidator.ERROR + "Укажите путь к файлу.");
+				System.out.println(ResourceValidator.ERROR + ERROR_MESSAGE);
 				return Integer.valueOf(ResourceValidator.ERROR_RESULT_CODE);
 			}
 			

61ec7bd2bbc96b84e00738cf7139227c6592b546
Sorcerer <Sorcerer@LAPTOP-G4MS948M>
2020-08-24 17:21:22 +0300

    Завершён класс для общего теста программы. Созданы файлы для теста.

--- dcmdon.resources.validation/src/test/java/dcmdon/resources/validation/test/ResourceValidatorTest.java
+++ dcmdon.resources.validation/src/test/java/dcmdon/resources/validation/test/ResourceValidatorTest.java
@@ -1,24 +1,36 @@
 package dcmdon.resources.validation.test;
 
-//import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-//import org.junit.Assert;
 import org.junit.Test;
 
-//import dcmdon.resources.validation.ResourceValidator;
+import dcmdon.resources.validation.ResourceValidator;
 
+/**
+ * Класс для общего теста программы
+ * (теста методов класса ResourceValidator).
+ */
 public class ResourceValidatorTest
 {
+	//Путь к файлу конфигурации:
+	private final String m_configFilePath = "src/test/resources/config.json";
+	
+	/**
+	 * Вызывает метод "validateAndGetReport"
+	 * класса ResourceValidator и проверяет
+	 * код, получаемый с помощью метода 
+	 * "getValidationResultCode()", на равенство
+	 * коду отсутствия любых ошибок во время
+	 * выполнения программы проверки файлов,
+	 * указанных в файле конфигурации.
+	 * @throws Exception
+	 */
 	@Test
 	public void test() throws Exception
 	{
-		/*String filePath = "C:\\Users\\Black\\Desktop\\tests\\config.json";
-		ResourceValidator validator = new ResourceValidator(filePath);
-		
-		
-		validator.validateAndGetReport();*/
-		
-		
-		
+		ResourceValidator validator = new ResourceValidator(m_configFilePath);
+		validator.validateAndGetReport();
+		assertEquals(validator.getValidationResultCode(),
+					           ResourceValidator.OK_RESULT_CODE);
 	}
 }

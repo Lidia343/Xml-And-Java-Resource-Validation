b2f1d9556485e7a69b7630a1608bd1dc9fcdf75f
Sorcerer <Sorcerer@LAPTOP-G4MS948M>
2020-08-25 17:04:10 +0300

    Добавлены ресурсы для тестов.

--- dcmdon.resources.validation/src/test/resources/invalid/property_interface1.java
+++ dcmdon.resources.validation/src/test/resources/invalid/property_interface1.java
@@ -2,11 +2,17 @@ package dcmdon.resources;
 
 public interface IProperties
 {
-	public final static short PROP_ID_CRC=60 ;// CRC
+	public final static short _ID_CRC=60 ;// CRC
+	
+	public final static short PROP_ID_CRC=97     ;//обладатель объекта
+	
+	public final static short RES_ID_CRC=999     ;//обладатель объекта
+
+	public final static short PROP_ID_NESTING			=97 ; //sc
 	
 	/*public final static short _ID_CRC=97     ;//обладатель объекта
 
-	public final static short _ID_CRC			=97 ; //sc
+	/*public final static short _ID_CRC			=97 ; //sc
 
 	public final static short _ID_CRC				=100; //*/
 
@@ -19,6 +25,8 @@ public interface IProperties
 	public final static short PROP_ID_TYPE				= 106 ;  // тип
 	//public final static short ID_REGIME			= 106 ;
 	//public final static short _ID_NUM				= 106 ;
+	public final static short ID_REGIME			= "value" ;
+	public final static short _ID_NUM				= 106346324 ;
 	public final static short PROP_ID_VALUE				= 121 ;	// значение
 	  //public final static short PROP_ID_NESTING			= 126 ; 	// код Nesting
 	public final static short PROP_ID_PROPERTIES		= 127 ; 	// свойства объекта
--- dcmdon.resources.validation/src/test/resources/invalid/property_interface2.java
+++ dcmdon.resources.validation/src/test/resources/invalid/property_interface2.java
@@ -2,13 +2,14 @@ package dcmdon.railway.resources;
 
 public interface IProperties extends dcmdon.resources.IProperties
 {
+	public final static short PROP_ID_PART		= 60;
 	public final static short PROP_ID_INDEX				= 104;	// индекс
 	public final static short PROP_ID_PLOTFRAME			= 105; 
-	public final static short PROP_ID_FREQUENCY			= 108;
+	public final static short _ID_FREQUENCY			= 108;
 	public final static short PROP_ID_SPEED				= 109;
 	public final static short PROP_ID_CHAIN_POINTS		= 111;	// точка цепи
-	public final static short PROP_ID_PLACE				= 112;
-	public final static short PROP_ID_CHAIN				= 113;
+	public final static short PROP_ID_PLACE				= 101;
+	public final static short PROP_ID_CHAIN				= 101;
 	public final static short PROP_ID_ADJACENT			= 114;  // смежный
 	public final static short PROP_ID_TRACK				= 115;  //
                    /*public final static short RES_ID_EDGE				= 117;

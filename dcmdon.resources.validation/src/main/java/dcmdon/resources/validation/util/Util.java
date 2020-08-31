01c51c0b46656b25fba2dfb048c5d9c1c551add5
дом <@169.254.170.118>
2020-08-31 17:44:04 +0300

    Добавлены комментарии к классу Util.

--- dcmdon.resources.validation/src/main/java/dcmdon/resources/validation/util/Util.java
+++ dcmdon.resources.validation/src/main/java/dcmdon/resources/validation/util/Util.java
@@ -3,11 +3,23 @@ package dcmdon.resources.validation.util;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Objects;
 
+/**
+ * Утилитарный класс.
+ */
 public class Util
 {
+	/**
+	 * @param a_reader
+	 * 		  Объект для чтения
+	 * @return строку, прочитанную с помощью
+	 * a_reader
+	 * @throws IOException
+	 */
 	public static String getText (BufferedReader a_reader) throws IOException
 	{
+		Objects.requireNonNull(a_reader);
 		StringBuilder result = new StringBuilder();
 		String line;
 		while ((line = a_reader.readLine()) != null)
@@ -17,8 +29,16 @@ public class Util
 		return result.toString();
 	}
 	
+	/**
+	 * @param a_filePath
+	 * 		  Файл для чтения
+	 * @return содержимое файла a_filePath в виде
+	 * строки
+	 * @throws IOException
+	 */
 	public static String getText (String a_filePath) throws IOException
 	{
+		Objects.requireNonNull(a_filePath);
 		try (BufferedReader reader = new BufferedReader(new FileReader(a_filePath)))
 		{
 			return getText(reader);
